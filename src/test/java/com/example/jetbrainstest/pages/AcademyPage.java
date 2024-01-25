aa050e4e1bbc56611360847cbcd17167c30bf416
twinkiart <slava.engels.2000@gmail.com>
2024-01-25 15:12:39 +0400

    AcademyTest allure (Добавлены 6 тестов )

--- src/test/java/com/example/jetbrainstest/pages/AcademyPage.java
+++ src/test/java/com/example/jetbrainstest/pages/AcademyPage.java
@@ -20,25 +20,36 @@ public class AcademyPage {
 
     @FindBy(css = "[data-test='button'][href*='email-sales']")
     private WebElement contactButton;
-
     @FindBy(xpath = "//h3[contains(text(),'Data science')]")
     private WebElement dataScienceButton;
-
     @FindBy(xpath = "//h4[contains(text(),'Java')]")
     private WebElement javaButton;
-
+    @FindBy(xpath = "//h4[contains(text(),'Python')]")
+    private WebElement pythonButton;
+    @FindBy(xpath = "//h4[contains(text(),'SQL')]")
+    private WebElement sqlButton;
+    @FindBy(xpath = "//a[contains(text(), 'Learn by doing')]")
+    private WebElement learnbd;
+    @FindBy(xpath = "//a[contains(text(), 'The JetBrains Academy blog')]")
+    private WebElement Blog;
+    @FindBy(xpath = "//a[contains(text(), '@JetBrains_Edu on X')]")
+    private WebElement Twitter;
+    @FindBy(xpath = "//a[contains(text(), 'Our bug and issue tracker')]")
+    private WebElement BugTracker;
     @FindBy(xpath = "//input[@placeholder='Email address']")
     private WebElement emailAddress;
     @FindBy(xpath = "//textarea[@placeholder='Your feedback']")
     private WebElement feedBack;
+
     @FindBy(css = "button[data-test='button'][type='submit']")
     private WebElement submitButton;
+
+
 
     public AcademyPage(WebDriver driver){
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
-
 
     public void clickScienceButton(){
         dataScienceButton.click();
@@ -53,13 +64,57 @@ public class AcademyPage {
         javaButton.click();
         LOG.infoWithScreenshot("Кликнули по кнопке Java");
     }
+    public  void clickPythonButton(){
+        pythonButton.click();
+        LOG.infoWithScreenshot("Кликнули по кнопке Python");
+    }
+    public void clickSqlButton(){
+        sqlButton.click();
+        LOG.infoWithScreenshot("Кликнули по кнопке SQL");
+    }
+    public void clickLearnBd(){
+        learnbd.click();
+        LOG.infoWithScreenshot("Кликнули по кнопке Learn by doing");
+    }
+    public void clickAcademyBlog(){
+        Blog.click();
+        LOG.infoWithScreenshot("Кликнули по кнопке The JetBrains Academy blog");
+    }
+    public void clickTwitter(){
+        Twitter.click();
+        LOG.infoWithScreenshot("Кликнули по кнопке @JetBrains_Edu on X");
+    }
+    public void clickBugTracker(){
+        BugTracker.click();
+        LOG.infoWithScreenshot("Кликнули по кнопке Our bug and issue tracker");
+
+
+    }
+
+    public String getPythontext(){
+        return driver.findElement(By.tagName("body")).getText();
+    }
+    public String getSqlText(){
+        return driver.findElement(By.tagName("body")).getText();
+    }
+
     public String getJavaText() {
         return driver.findElement(By.tagName("body")).getText();
     }
+    public String getLearText(){
+        return  driver.findElement(By.tagName("body")).getText();
+    }
+    public String getBlogText(){
+        return driver.findElement(By.tagName("body")).getText();
+    }
+
+
     public void enterEmail(String email){
         emailAddress.sendKeys(email);
         LOG.infoWithScreenshot("Email введен: " + email);
     }
+
+
     public void feedBack(String feedback){
         feedBack.sendKeys(feedback);
         LOG.infoWithScreenshot("Feedback введен: " +feedback);
@@ -68,8 +123,6 @@ public class AcademyPage {
         submitButton.click();
         LOG.infoWithScreenshot("Нажата кнопка Submit");
     }
-
-
 
     public String getCurrUrl() {
         return driver.getCurrentUrl();
--- src/test/java/com/example/jetbrainstest/tests/AcademyTest.java
+++ src/test/java/com/example/jetbrainstest/tests/AcademyTest.java
@@ -1,8 +1,6 @@
 package com.example.jetbrainstest.tests;
 
-import com.example.jetbrainstest.AllureAttachmentsManager;
 import com.example.jetbrainstest.pages.AcademyPage;
-import com.example.jetbrainstest.pages.spacepages.SpacePage;
 import io.qameta.allure.Step;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -44,15 +42,6 @@ public class AcademyTest extends BaseTest {
     }
 
     @Test
-    @DisplayName("Клик по кнопке Java")
-    public void clickJavaButton() {
-        academyPage.clickJavaButton();
-        assertEquals("https://academy.jetbrains.com/?tag=Java&_gl=1", academyPage.getCurrUrl(), "Не перешли на https://academy.jetbrains.com/?tag=Java&_gl");
-        String pageText = academyPage.getJavaText();
-        assertTrue(pageText.contains("JAVA"), "Текст 'JAVA' не найден на странице");
-    }
-
-    @Test
     @DisplayName("Ввод email,feedback и клик по кнопке Submit")
     public void enterEmailAndClickSubmit() {
         String testEmail = "testemail@maill.com";
@@ -64,5 +53,62 @@ public class AcademyTest extends BaseTest {
         boolean isMessageDisplayed = wait.until(ExpectedConditions.textToBePresentInElementLocated(By.xpath("//p[contains(text(),'Thanks for your request!')]"), "Thanks for your request!"));
         assertTrue(isMessageDisplayed, "Сообщение 'Thanks for your request!' не появилось.");
     }
+
+    @Test
+    @DisplayName("Клик по кнопке Python")
+    public void clickPythonButton(){
+        academyPage.clickPythonButton();
+        assertEquals("https://academy.jetbrains.com/?tag=Python&_gl=1", academyPage.getCurrUrl(),"Не перешли на https://academy.jetbrains.com/?tag=Python&_gl");
+        String pageText = academyPage.getPythontext();
+        assertTrue(pageText.contains("Python"), "Текст 'Python' не найден на странице");
+    }
+    @Test
+    @DisplayName("Клик по кнопке SQL")
+    public void clickSqlButton(){
+        academyPage.clickSqlButton();
+        assertEquals("https://hyperskill.org/tracks?category=9&_gl=1", academyPage.getCurrUrl(),"Не перешли на https://hyperskill.org/tracks?category=9&_gl=1");
+        String pageText = academyPage.getSqlText();
+        assertTrue(pageText.contains("SQL and Databases"), "Текст 'SQL and Databases' не найден на странице");
+    }
+    @Test
+    @DisplayName("Клик по кнопке Java")
+    public void clickJavaButton() {
+        academyPage.clickJavaButton();
+        assertEquals("https://academy.jetbrains.com/?tag=Java&_gl=1", academyPage.getCurrUrl(), "Не перешли на https://academy.jetbrains.com/?tag=Java&_gl");
+        String pageText = academyPage.getJavaText();
+        assertTrue(pageText.contains("JAVA"), "Текст 'JAVA' не найден на странице");
+    }
+    @Test
+    @DisplayName("Клик по кнопке Learn by doing")
+    public void clickLeranBd(){
+        academyPage.clickLearnBd();
+        assertEquals("https://hyperskill.org/tracks?", academyPage.getCurrUrl(),"Не перешли на https://hyperskill.org/tracks?_gl=");
+        String pageText = academyPage.getLearText();
+        assertTrue(pageText.contains(" All tracks "),"Текст 'All tracks' не найден на странице");
+    }
+    @Test
+    @DisplayName("Клик по кнопке The JetBrains Academy blog")
+    public void clickAcademyblog(){
+        academyPage.clickAcademyBlog();
+        assertEquals("https://blog.jetbrains.com/education/",academyPage.getCurrUrl(),"Не перешли на https://blog.jetbrains.com/education");
+        String pageText = academyPage.getBlogText();
+        assertTrue(pageText.contains("The JetBrains Academy Blog"),"Текст 'The JetBrains Academy Blog' не найден на странице");
+    }
+    @Test
+    @DisplayName("Клик по кнопке @JetBrains_Edu on X")
+    public void twitterPageBlog(){
+        academyPage.clickTwitter();
+        assertEquals("https://twitter.com/JetBrains_Edu", academyPage.getCurrUrl(),"Не перешли в twitter");
+    }
+    @Test
+    @DisplayName("Клик по кнопке Our bug and issue tracker")
+    public void bugTracker(){
+        academyPage.clickBugTracker();
+        assertEquals("https://youtrack.jetbrains.com/issues?q=project:%20%7BEDU%7D&_gl=1/",academyPage.getCurrUrl(),"Не перешли в https://youtrack.jetbrains.com/");
+    }
+
+
+
+
 }
 
