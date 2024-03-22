56a4e9b24a2703935e500556f64890ac23c662a2
Barkatina <159892946+Barkatina@users.noreply.github.com>
2024-03-22 15:37:49 +0300

    Merge remote-tracking branch 'origin/ScalaPage2' into ScalaPage2

--- src/test/java/com/example/jetbrainstest/tests/ScalaTest.java
+++ src/test/java/com/example/jetbrainstest/tests/ScalaTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @ExtendWith(MyExtension.class)
 public class ScalaTest extends BaseTest {
     private ScalaPage scalaPage;
