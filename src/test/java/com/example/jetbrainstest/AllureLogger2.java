1f775107f6612b81a0117e5393a24240dd02123c
Ilya Poplavsky <i.poplavsky@paynocchio.com>
2024-05-02 11:17:29 +0300

    Fixed AllureLogger2.java and RustRoverPage.java

--- src/test/java/com/example/jetbrainstest/AllureLogger2.java
+++ src/test/java/com/example/jetbrainstest/AllureLogger2.java
@@ -1,14 +1,17 @@
 package com.example.jetbrainstest;
+
 import io.qameta.allure.Step;
 import org.slf4j.Logger;
 
 public class AllureLogger2 {
     private Logger logger;
-    public AllureLogger2 (Logger logger) {
+
+    public AllureLogger2(Logger logger) {
         this.logger = logger;
     }
+
     @Step
-    public void info (String logText) {
+    public void info(String logText) {
         logger.info(logText);
     }
 
--- src/test/java/com/example/jetbrainstest/pages/RustRoverPage.java
+++ src/test/java/com/example/jetbrainstest/pages/RustRoverPage.java
@@ -1,5 +1,6 @@
 package com.example.jetbrainstest.pages;
 
+import com.example.jetbrainstest.AllureLogger2;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -8,27 +9,28 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 //https://www.jetbrains.com/rust/
-public class RustRoverPage
-{
-    private final Logger LOG = LoggerFactory.getLogger(RustRoverPage.class);
+public class RustRoverPage {
+    private final Logger LOG = LoggerFactory.getLogger(AllureLogger2.class);
     WebDriver driver;
     @FindBy(css = "a[href='/rust/download/']")
     private WebElement downloadRustPreview;
     @FindBy(css = "button[aria-label='Education: Open submenu']")
     private WebElement educationDropDown;
 
-    public String clickRustDownloadAndCheckUrl(){
+    public String clickRustDownloadAndCheckUrl() {
         LOG.info("Checking if URL changed after button has been clicked");
         driver.get("https://www.jetbrains.com/rust/");
         downloadRustPreview.click();
         return driver.getCurrentUrl();
     }
+
     public void getEducationPage() {
         educationDropDown.click();
         LOG.info("Clicking on education link");
     }
-    public RustRoverPage(WebDriver driver){
+
+    public RustRoverPage(WebDriver driver) {
         this.driver = driver;
-        PageFactory.initElements(driver,this);
+        PageFactory.initElements(driver, this);
     }
 }
