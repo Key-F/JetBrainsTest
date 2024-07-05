10bf43f2dcd9b89d4492b097d5edbdeeb7232648
Github Krotov <GithubKrotov@yandex.ru>
2024-07-05 22:53:20 +0300

    -adding logging into tests

--- src/test/java/com/example/jetbrainstest/pages/bigDataToolsPage.java
+++ src/test/java/com/example/jetbrainstest/pages/bigDataToolsPage.java
@@ -5,8 +5,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class bigDataToolsPage {
+    private final Logger LOG = LoggerFactory.getLogger(bigDataToolsPage.class);
     WebDriver driver;
     private WebDriverWait wait;
 
@@ -14,6 +17,7 @@ public class bigDataToolsPage {
     private WebElement getButton;
 
     public Boolean checkIfGetButtonIsClickable() {
+        LOG.info("Проверка активности кнопки скачивания");
         return getButton.isEnabled();
     }
 
