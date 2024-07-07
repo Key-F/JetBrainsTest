e276a86469f7817be3c36d25fb4f21928ee220c2
kaggy <kaggy8@ya.ru>
2024-07-07 18:42:45 +0300

    slf4j added

--- src/test/java/com/example/jetbrainstest/pages/resharper/ReSharperPage.java
+++ src/test/java/com/example/jetbrainstest/pages/resharper/ReSharperPage.java
@@ -4,12 +4,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 // https://www.jetbrains.com/resharper/
 
 public class ReSharperPage {
+
+    private final Logger LOG = LoggerFactory.getLogger(ReSharperPage.class);
 
     @FindBy(css = "a[href='/resharper/download/']")
     private WebElement downloadLink;
@@ -21,17 +25,17 @@ public class ReSharperPage {
     private List<WebElement> resourcesLinks;
 
     public Boolean downloadButtonDisplay() {
-        System.out.println("Проверка наличия кнопки Download");
+        LOG.info("Проверка наличия кнопки Download");
         return downloadLink.isDisplayed();
     }
 
     public String checkEmailFieldPlaceholder() {
-        System.out.println("Проверка верного placeholder в поле Email");
+        LOG.info("Проверка верного placeholder в поле Email");
         return emailField.getAttribute("placeholder");
     }
 
     public Boolean checkBlogLinkInResources(String linkOnBlog) {
-        System.out.println("Проверка наличия ссылки на Blog в подвале страницы");
+        LOG.info("Проверка наличия ссылки на Blog в подвале страницы");
         for (int i = 0; i < resourcesLinks.size() - 1; i++) {
             if (resourcesLinks.get(i).getAttribute("href").equals(linkOnBlog)) {
                 return true;
