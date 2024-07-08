770348ddc224525dc0af01dda0213f9557658d72
Github Krotov <GithubKrotov@yandex.ru>
2024-07-08 12:06:08 +0300

    -adding AllureLogger to the project

--- src/test/java/com/example/jetbrainstest/pages/BigDataToolsPage.java
+++ src/test/java/com/example/jetbrainstest/pages/BigDataToolsPage.java
@@ -1,5 +1,6 @@
 package com.example.jetbrainstest.pages;
 
+import com.example.jetbrainstest.AllureLogger;
 import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,14 +11,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BigDataToolsPage {
-    private final Logger LOG = LoggerFactory.getLogger(BigDataToolsPage.class);
+    private final AllureLogger LOG = new AllureLogger(LoggerFactory.getLogger(BigDataToolsPage.class));
     WebDriver driver;
     private WebDriverWait wait;
 
     @FindBy(xpath = "//*[contains(text(),'Get')]")
     private WebElement getButton;
 
-    @Step("Проверка активности кнопки скачивания")
     public Boolean checkIfGetButtonIsClickable() {
         LOG.info("Проверка активности кнопки скачивания");
         return getButton.isEnabled();
--- src/test/java/com/example/jetbrainstest/tests/BigDataToolsTest.java
+++ src/test/java/com/example/jetbrainstest/tests/BigDataToolsTest.java
@@ -8,19 +8,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BigDataToolsTest extends BaseTest {
-    private BigDataToolsPage bigDataToolsPage;
+    private BigDataToolsPage BigDataToolsPage;
 
     @BeforeEach
     @Override
     public void setUp() {
         super.setUp();
         getDriver().get("https://plugins.jetbrains.com/plugin/12494-big-data-tools");
-        bigDataToolsPage = new BigDataToolsPage(getDriver());
+        BigDataToolsPage = new BigDataToolsPage(getDriver());
     }
 
     @Test
     @DisplayName("Проверка, что кнопка скачивания активна")
     public void buttonCheck() {
-        assertTrue(bigDataToolsPage.checkIfGetButtonIsClickable(), "Кнопка скачивания не активна");
+        assertTrue(BigDataToolsPage.checkIfGetButtonIsClickable(), "Кнопка скачивания не активна");
     }
 }
