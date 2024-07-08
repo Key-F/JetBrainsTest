7f4ea88cea5ff70d0398daf60488e6e222dd125f
Github Krotov <GithubKrotov@yandex.ru>
2024-07-08 13:22:23 +0300

    -adding screenshot to the Test step

--- src/test/java/com/example/jetbrainstest/pages/BigDataToolsPage.java
+++ src/test/java/com/example/jetbrainstest/pages/BigDataToolsPage.java
@@ -1,13 +1,11 @@
 package com.example.jetbrainstest.pages;
 
 import com.example.jetbrainstest.AllureLogger;
-import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BigDataToolsPage {
@@ -19,7 +17,7 @@ public class BigDataToolsPage {
     private WebElement getButton;
 
     public Boolean checkIfGetButtonIsClickable() {
-        LOG.info("Проверка активности кнопки скачивания");
+        LOG.infoWithScreenshot("Проверка активности кнопки скачивания");
         return getButton.isEnabled();
     }
 
