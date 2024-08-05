557f4e001fbda7c2f5c684fc9e7de7ff92702894
qavlg <aleksss900@gmail.com>
2024-08-05 11:48:49 +0300

    the correction of FindBy

--- src/test/java/com/example/jetbrainstest/pages/RiderFlowPage.java
+++ src/test/java/com/example/jetbrainstest/pages/RiderFlowPage.java
@@ -1,5 +1,8 @@
 package com.example.jetbrainstest.pages;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -14,19 +17,21 @@ import java.time.Duration;
 
 public class RiderFlowPage {
 
+    private final Logger LOG = LoggerFactory.getLogger(RiderFlowPage.class);
+
     WebDriver driver;
     String message = "clicked";
 
     @FindBy(css = "div > nav > div:nth-child(1) > button")
     private WebElement riderFlowPage;
 
-    @FindBy(css = "#wt-site-header > div > div > div.wt-row.wt-row_align-items_center.wt-row_size_0 > div.wt-col-auto-fill.wt-col_align-self_stretch._siteHeader__contentPart_pisqy._siteHeader__desktopContentPart_cidj1._siteHeader__desktopContentPart_83ifal > div > nav > div._mainMenuItem_buxog._mainMenuItemActive_ol6e5j._mainMenuItemActive_kegas._mainMenuItemActive_z4ja9._mainMenuItemActive_h9omn._mainMenuItemFirstActive_x76mu > div > div > div > div._mainSubmenu__content_cx5o._mainSubmenu__content_ce97v > div._mainSubmenu__columnsWrapper_ex4h > div:nth-child(2) > div > div:nth-child(6) > a > span > span")
+    @FindBy(xpath = "//span[contains(text(),'RiderFlow')][last()]")
     private WebElement developerToolsElement;
 
-    @FindBy(css = "body > div.jetbrains-cookies-banner-4 > div.jetbrains-cookies-banner-4__body > div:nth-child(2) > div > button.jetbrains-cookies-banner-4-button.jetbrains-cookies-banner-4-button--accent.jetbrains-cookies-banner-4__action\n")
+    @FindBy(xpath = "//button[@data-jetbrains-cookies-banner-action='ACCEPT_ALL']")
     private WebElement cookieAcceptButton;
 
-    @FindBy(xpath = "//*[@id='riderflow-page']/section[1]/div/div/div[2]/div/a")
+    @FindBy(css = "a[href='/riderflow/download']:first-child")
     private WebElement watchButton;
 
     @FindBy(xpath = "//h1[contains(text(),'RiderFlow for Unity')]")
@@ -51,6 +56,7 @@ public class RiderFlowPage {
 
     public void clickCookieAcceptButton() {
         cookieAcceptButton.click();
+      LOG.info("Action of AcceptButton: ");
         System.out.println("Action of AcceptButton: " + message);
     }
 
