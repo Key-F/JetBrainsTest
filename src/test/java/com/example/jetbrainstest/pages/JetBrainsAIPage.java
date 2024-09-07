e683086eb9a63382a6907ecdcbaaa5b444e9d5d9
Simon <work.sergey@list.ru>
2024-09-07 21:03:35 +0300

    add tests

--- src/test/java/com/example/jetbrainstest/pages/JetBrainsAIPage.java
+++ src/test/java/com/example/jetbrainstest/pages/JetBrainsAIPage.java
@@ -1,36 +1,33 @@
 package com.example.jetbrainstest.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+
+
 public class JetBrainsAIPage {
 
     private WebDriver driver;
 
-    @FindBy(css = "[class*=\"wt-row\"] > [href=\"/idea/\"]")
-    private WebElement ideaButton;
+    @FindBy(css = "[aria-label='Close cookies banner']")
+    private WebElement closeCookiesBanner;
 
-    @FindBy(css = "[class*=\"wt-row\"] > [href=\"/pycharm/\"]")
-    private WebElement pyCharmButton;
-
-    @FindBy(css = "[class*=\"wt-row\"] > [href=\"/phpstorm/\"]")
-    private WebElement phpStormButton;
-
-    @FindBy(css = "[class*=\"wt-row\"] > [href=\"/resharper/\"]")
-    private WebElement reSharperButton;
-
-    @FindBy(css = "[class*=\"wt-row\"] > [href=\"/fleet/\"]")
-    private WebElement fleetButton;
-
-    @FindBy(css = "[class*=\"wt-row\"] > [href=\"/ides/\"]")
-    private WebElement allIdesButton;
-
-    public void clickButton(String button){
-
+    public void setCloseCookiesBanner(){
+        closeCookiesBanner.click();
     }
 
+    public String getButtonCssSelector(String buttonName){
+        String cssSelector = "[class *= 'wt-row'] > [href = '/" + buttonName + "/']";
+        return cssSelector;
+    }
+
+    public void clickButton(String buttonName){
+        WebElement button = driver.findElement(By.cssSelector(getButtonCssSelector(buttonName)));
+        button.click();
+    }
 
     public JetBrainsAIPage(WebDriver driver){
         this.driver = driver;
