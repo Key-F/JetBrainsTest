9887cc9dc59b6bf5232067b78ca8c42e1828e744
Simon <work.sergey@list.ru>
2024-09-08 15:26:11 +0300

    add test and logger

--- src/test/java/com/example/jetbrainstest/pages/JetBrainsAIPage.java
+++ src/test/java/com/example/jetbrainstest/pages/JetBrainsAIPage.java
@@ -5,33 +5,52 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class JetBrainsAIPage {
 
+    private final Logger LOG = LoggerFactory.getLogger(JetBrainsAIPage.class);
     private WebDriver driver;
+
+    public JetBrainsAIPage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
 
     @FindBy(css = "[aria-label='Close cookies banner']")
     private WebElement closeCookiesBanner;
 
+    @FindBy(css = "[name = 'email']")
+    private WebElement emailField;
+
     public void setCloseCookiesBanner(){
+        LOG.info("Закрытие баннера с принятием cookies");
         closeCookiesBanner.click();
     }
 
     public String getButtonCssSelector(String buttonName){
+        LOG.info("Получение CSS селектора элемента " + buttonName );
         String cssSelector = "[class *= 'wt-row'] > [href = '/" + buttonName + "/']";
         return cssSelector;
     }
 
     public void clickButton(String buttonName){
+        LOG.info("Нажатие на элемент " + buttonName);
         WebElement button = driver.findElement(By.cssSelector(getButtonCssSelector(buttonName)));
         button.click();
     }
 
-    public JetBrainsAIPage(WebDriver driver){
-        this.driver = driver;
-        PageFactory.initElements(driver, this);
+    public void enterEmail(String email) {
+        LOG.info("В поле ввода адреса электронной почты введён следующий email: " + email);
+        emailField.sendKeys(email);
+    }
+
+    public String getEmailFieldValue(){
+        LOG.info("Получен атрибут 'value' поля ввода адреса электронной почты");
+        String value = emailField.getAttribute("value");
+        return value;
     }
 
 }
