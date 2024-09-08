038e99594fa81a63f23a733910ce3180d9faf58a
Simon <work.sergey@list.ru>
2024-09-08 16:19:22 +0300

    add allure reports

--- src/test/java/com/example/jetbrainstest/pages/JetBrainsAiPage.java
+++ src/test/java/com/example/jetbrainstest/pages/JetBrainsAiPage.java
@@ -1,5 +1,6 @@
 package com.example.jetbrainstest.pages;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -25,28 +26,33 @@ public class JetBrainsAiPage {
     @FindBy(css = "[name = 'email']")
     private WebElement emailField;
 
+    @Step("Закрытие баннера с принятием cookies")
     public void setCloseCookiesBanner(){
         LOG.info("Закрытие баннера с принятием cookies");
         closeCookiesBanner.click();
     }
 
+    @Step("Получение CSS селектора элемента")
     public String getButtonCssSelector(String buttonName){
         LOG.info("Получение CSS селектора элемента " + buttonName );
         String cssSelector = "[class *= 'wt-row'] > [href = '/" + buttonName + "/']";
         return cssSelector;
     }
 
+    @Step("Нажатие на элемент")
     public void clickButton(String buttonName){
         LOG.info("Нажатие на элемент " + buttonName);
         WebElement button = driver.findElement(By.cssSelector(getButtonCssSelector(buttonName)));
         button.click();
     }
 
+    @Step("Ввод в поле адреса электронной почты")
     public void enterEmail(String email) {
         LOG.info("В поле ввода адреса электронной почты введён следующий email: " + email);
         emailField.sendKeys(email);
     }
 
+    @Step("Получение атрибута поля ввода")
     public String getEmailFieldValue(){
         LOG.info("Получен атрибут 'value' поля ввода адреса электронной почты");
         String value = emailField.getAttribute("value");
