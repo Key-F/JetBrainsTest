e0e187b0bef221952d6f560b4df203c00eaab9b4
EternalDepression <blackshift99@mail.ru>
2024-09-09 16:26:01 +0300

    Changes Added.

--- src/test/java/com/example/jetbrainstest/tests/DotCoverTest.java
+++ src/test/java/com/example/jetbrainstest/tests/DotCoverTest.java
@@ -5,9 +5,11 @@ import io.qameta.allure.Step;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@ExtendWith(MyExtension.class)
 public class DotCoverTest extends BaseTest {
 
     private DotCoverPage DotCoverPage;
