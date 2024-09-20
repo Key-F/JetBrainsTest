8061727d0cd0f09fa0a7f9502e549c82e4f88e5f
FlashFromFuture <techee@mail.ru>
2024-09-20 13:40:54 +0400

    add logger

--- src/test/java/com/example/jetbrainstest/pages/DotPeekPage.java
+++ src/test/java/com/example/jetbrainstest/pages/DotPeekPage.java
@@ -4,10 +4,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 // https://www.jetbrains.com/decompiler/
 
 public class DotPeekPage {
+
+    private final Logger LOG  = LoggerFactory.getLogger(DotPeekPage.class);
 
     WebDriver driver;
 
@@ -21,7 +25,7 @@ public class DotPeekPage {
     }
 
     public Boolean checkDownloadButtonIsClickable(){
-
+        LOG.info("Проверка активности кнопки загрузки");
         return downloadButton.isEnabled();
     }
 
