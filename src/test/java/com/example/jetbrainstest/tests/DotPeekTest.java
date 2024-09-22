55a271ce8ed2ca5514cf30ef61a482786c934a2f
FlashFromFuture <techee@mail.ru>
2024-09-22 12:56:33 +0400

    add a afterTestExecutionCallback for error screenShots

--- src/test/java/com/example/jetbrainstest/tests/DotPeekTest.java
+++ src/test/java/com/example/jetbrainstest/tests/DotPeekTest.java
@@ -1,18 +1,22 @@
 package com.example.jetbrainstest.tests;
+
+import com.example.jetbrainstest.MyExtension;
 import com.example.jetbrainstest.pages.DotPeekPage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@ExtendWith(MyExtension.class)
 public class DotPeekTest extends BaseTest {
 
     private DotPeekPage DotPeekPage;
 
     @BeforeEach
     @Override
-    public void setUp(){
+    public void setUp() {
         super.setUp();
         getDriver().get("https://www.jetbrains.com/decompiler/");
         DotPeekPage = new DotPeekPage(getDriver());
@@ -20,7 +24,7 @@ public class DotPeekTest extends BaseTest {
 
     @Test
     @DisplayName("Проверка, что кнопка скачивания активна")
-    public void buttonCheck(){
-        assertTrue(DotPeekPage.checkDownloadButtonIsClickable(),"Button is not ready!");
+    public void buttonCheck() {
+        assertTrue(DotPeekPage.checkDownloadButtonIsClickable(), "Button is not ready!");
     }
 }
